acf49ffdf8224114bc3d16ea50c2c1e1c32fe21a
Nitish kumar <nitish.edu.in@gmail.com>
2024-03-18 20:31:37 +0530

    edit

--- LeetCode/Q_2469_ConvertTemp.cpp
+++ LeetCode/Q_2469_ConvertTemp.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 using namespace std;
 
-vector<double> convertTemperature(double celsius){
+vector<double> convertTemperature(double celsius)
+{
    vector<double> vct;
    double Kelvin = celsius + 273.15;
    vct.push_back(Kelvin);
@@ -12,12 +13,14 @@ vector<double> convertTemperature(double celsius){
    return vct;
 }
 
-int main(){
+int main()
+{
    double celsius;
    cout << "Enter the celsius :";
-   cin>>celsius;
+   cin >> celsius;
    vector<double> vct = convertTemperature(celsius);
-   for (int i = 0; i < vct.size(); i++) {
-        cout << vct[i] << ", ";
-    }
+   for (int i = 0; i < vct.size(); i++)
+   {
+      cout << vct[i] << ", ";
+   }
 }
