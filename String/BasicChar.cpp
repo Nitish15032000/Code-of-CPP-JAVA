215944ce6c29181244e8edaf01985079540e4373
Nitish kumar <nitish.edu.in@gmail.com>
2024-03-30 19:58:07 +0530

    char

---
+++ String/BasicChar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   char ch[10];
+
+   // Nitish Kumar --> Nitish (only printed)
+   cout << "Enter the char : ";
+   cin >> ch;
+   cout << ch << endl;
+   
+   
+   // Nitish Kumar --> Nitish Kumar
+   cout << "Enter the char : ";
+   cin >> ch;
+   cout << ch << endl;
+
+   
+}
